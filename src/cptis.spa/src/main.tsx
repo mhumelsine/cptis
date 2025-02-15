ab6eb96c7afcb02418e79978f28887a1b993020d
Michael Humelsine <mhumelsine@isf.com>
2025-02-15 15:07:11 -0500

    notification position

--- src/cptis.spa/src/main.tsx
+++ src/cptis.spa/src/main.tsx
@@ -19,7 +19,7 @@ const theme = createTheme({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <Notifications />
+      <Notifications position="top-right" />
       <Container fluid>
         <DatesProvider
           settings={{
